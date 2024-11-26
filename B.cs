e3e08a90e7eb43bfc112d830bc719bfe374a5ec3
zhuangjunqi <zhuangjunqi@bilibili.com>
2024-11-26 11:01:31 +0800

    Add B

--- B.cs
+++ B.cs
@@ -7,7 +7,7 @@ public class B : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("B");
     }
 
     // Update is called once per frame
